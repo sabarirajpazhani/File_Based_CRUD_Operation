476f1d1fdcd392a8037bf835494c0b2fdd28668a
sabarirajp <sabarirajpazhani@gmail.com>
2025-05-12 11:43:29 +0530

    Adding Employee , View all the employee and View the particular employee

--- File_Based_CRUD_Operation/Program.cs
+++ File_Based_CRUD_Operation/Program.cs
@@ -2,129 +2,273 @@
 
 namespace File_Based_CRUD_Operation
 {
+    
     public class Program
     {
         public static int EmpID = 104;
         static void Main(string[] args)
-        {
-            Console.ForegroundColor = ConsoleColor.DarkCyan;
-            Console.WriteLine("---------------------- File Based Curd Operation (Employee Management) ----------------------");
-            Console.ResetColor();
-            Console.WriteLine();
-            Console.WriteLine("                                1. Add the Employee                                          ");
-            Console.WriteLine("                                2. View all the Employee                                     ");
-            Console.WriteLine("                                3. View Particular Employee By ID                            ");
-            Console.WriteLine("                                4. Update the Employee                                       ");
-            Console.WriteLine("                                5. Delete the Employee                                       ");
-            Console.WriteLine("                                6. Exit                                                      ");
-            Console.WriteLine();
-            Console.ForegroundColor = ConsoleColor.DarkCyan;
-            Console.WriteLine("---------------------------------------------------------------------------------------------");
-            Console.ResetColor();
+        {  
 
-            int Choice = 0;
-
-            string FilePath = @"D:\FileHandling\Employee.txt";
-
-            Choice:
-            try
+            while (true)
             {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.Write("Enter the Choice : ");
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                Console.WriteLine("---------------------- File Based Curd Operation (Employee Management) ----------------------");
                 Console.ResetColor();
-                int choice = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+                Console.WriteLine("                                1. Add the Employee                                          ");
+                Console.WriteLine("                                2. View all the Employee                                     ");
+                Console.WriteLine("                                3. View Particular Employee By ID                            ");
+                Console.WriteLine("                                4. Update the Employee                                       ");
+                Console.WriteLine("                                5. Delete the Employee                                       ");
+                Console.WriteLine("                                6. Exit                                                      ");
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                Console.WriteLine("---------------------------------------------------------------------------------------------");
+                Console.ResetColor();
 
-                if (choice == 0)
+
+                int Choice = 0;
+
+                string FilePath = @"D:\FileHandling\Employee.txt";
+
+                Choice:
+                try
                 {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Invalid Choice! Choice must not be Zero.");
-                    Console.ResetColor();
-                    goto Choice;
-                }
-                if (choice > 6)
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Invalid Choice! Please enter a number between 1 and 6.");
-                    Console.ResetColor();
-                    goto Choice;
-                }
-
-                Choice = choice;
-            }
-            catch (FormatException e)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Enter the choice using digits only—no letters, symbols, or whitespace.");
-                Console.ResetColor();
-                goto Choice;
-            }
-            catch (OverflowException e)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Invalid choice. Please enter a valid option.");
-                Console.ResetColor();
-                goto Choice;
-            }
-
-            switch (Choice)
-            {
-                case 1:
                     Console.ForegroundColor = ConsoleColor.Green;
-                    Console.Write("Enter the Employee Name : ");
+                    Console.Write("Enter the Choice : ");
                     Console.ResetColor();
-                    string EmployeeName = Console.ReadLine();
+                    int choice = int.Parse(Console.ReadLine());
 
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.Write("Enter the Department : ");
-                    Console.ResetColor();
-                    string Department = Console.ReadLine();
-
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.Write("Enter the Salary : ");
-                    Console.ResetColor();
-                    int Salary = int.Parse(Console.ReadLine());
-
-                    EmpID++;
-
-                    string details = $"{EmpID},{EmployeeName},{Department},{Salary}";
-
-                    if (File.Exists(FilePath))
-                    {
-                        using (StreamWriter sw = File.AppendText(FilePath))
-                        {
-                            sw.WriteLine(details);
-
-                            Console.ForegroundColor = ConsoleColor.DarkYellow;
-                            Console.WriteLine("                 * --------------- Registered Student --------------- *                      ");
-                            Console.ResetColor();
-
-                            Console.WriteLine($"                              Employee ID     : {EmpID}");
-                            Console.WriteLine($"                              Employee Name   : {EmployeeName}");
-                            Console.WriteLine($"                              Employe Deparment : {Department}");
-                            Console.WriteLine($"                              Emloyee Salary    : {Salary}");
-
-                            Console.ForegroundColor = ConsoleColor.DarkYellow;
-                            Console.WriteLine("                 ------------------------------------------------------                      ");
-                            Console.ResetColor();
-
-                            Console.WriteLine();
-                            Console.ForegroundColor = ConsoleColor.Blue;
-                            Console.WriteLine($"Employee ID -{EmpID} is Successfully Added");
-                            Console.WriteLine();
-                            Console.ResetColor();
-                        }
-                    }
-                    else
+                    if (choice == 0)
                     {
                         Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("File Not Found");
+                        Console.WriteLine("Invalid Choice! Choice must not be Zero.");
                         Console.ResetColor();
+                        goto Choice;
+                    }
+                    if (choice > 6)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Invalid Choice! Please enter a number between 1 and 6.");
+                        Console.ResetColor();
+                        goto Choice;
                     }
 
-                    break;
+                    Choice = choice;
+                }
+                catch (FormatException e)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Enter the choice using digits only—no letters, symbols, or whitespace.");
+                    Console.ResetColor();
+                    goto Choice;
+                }
+                catch (OverflowException e)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Invalid choice. Please enter a valid option.");
+                    Console.ResetColor();
+                    goto Choice;
+                }
+
+                switch (Choice)
+                {
+                    case 1:
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        Console.WriteLine("                              Enter '1' to create a new employee                             ");
+                        Console.ResetColor();
+                        Console.WriteLine();
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine("---------------------------------------------------------------------------------------------");
+                        Console.ResetColor();
+                        Console.WriteLine();
+
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write("Enter the Employee Name : ");
+                        Console.ResetColor();
+                        string EmployeeName = Console.ReadLine();
+
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write("Enter the Department : ");
+                        Console.ResetColor();
+                        string Department = Console.ReadLine();
+
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write("Enter the Salary : ");
+                        Console.ResetColor();
+                        int Salary = int.Parse(Console.ReadLine());
+
+                        EmpID++;
+
+                        string details = $"{EmpID},{EmployeeName},{Department},{Salary}";
+
+                        if (File.Exists(FilePath))
+                        {
+                            using (StreamWriter sw = File.AppendText(FilePath))
+                            {
+                                sw.WriteLine(details);
+
+                                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                                Console.WriteLine("                 * --------------- Registered Student --------------- *                      ");
+                                Console.ResetColor();
+
+                                Console.WriteLine($"                              Employee ID     : {EmpID}");
+                                Console.WriteLine($"                              Employee Name   : {EmployeeName}");
+                                Console.WriteLine($"                              Employe Deparment : {Department}");
+                                Console.WriteLine($"                              Emloyee Salary    : {Salary}");
+
+                                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                                Console.WriteLine("                 ------------------------------------------------------                      ");
+                                Console.ResetColor();
+
+                                Console.WriteLine();
+                                Console.ForegroundColor = ConsoleColor.Blue;
+                                Console.WriteLine($"Employee ID -{EmpID} is Successfully Added");
+                                Console.WriteLine();
+                                Console.ResetColor();
+                            }
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("File Not Found");
+                            Console.ResetColor();
+                        }
+
+                        break;
+
+
+                    case 2:
+
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        Console.WriteLine("                              Enter '2' to View all the Employee                             ");
+                        Console.ResetColor();
+                        Console.WriteLine();
+                        Console.ForegroundColor = ConsoleColor.DarkCyan;
+                        Console.WriteLine("---------------------------------------------------------------------------------------------");
+                        Console.ResetColor();
+                        Console.WriteLine();
+
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        Console.WriteLine("                                Here are the Employee details                                ");
+
+                        if (File.Exists(FilePath))
+                        {
+                            Console.ForegroundColor = ConsoleColor.DarkYellow;
+                            Console.WriteLine("---------------------------------------------------------------------------------------------");
+                            Console.ResetColor();
+
+                            Console.WriteLine(
+                                $"{"Employee ID",-20}" +
+                                $"{"Employee Name",-25}" +
+                                $"{"Department",-20}" +
+                                $"{"Salary",-15}"
+                            );
+
+                            Console.ForegroundColor = ConsoleColor.DarkYellow;
+                            Console.WriteLine("---------------------------------------------------------------------------------------------");
+                            Console.ResetColor();
 
 
 
+                            foreach (string list in File.ReadLines(FilePath))
+                            {
+                                string[] EmpDetails = list.Split(',');
+
+                                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                                Console.WriteLine(
+                                       $"{EmpDetails[0],-20}" +
+                                       $"{EmpDetails[1],-25}" +
+                                       $"{EmpDetails[2],-20}" +
+                                       $"{EmpDetails[3],-15}"
+                                );
+                                Console.ResetColor();
+                            }
+
+
+                            Console.ForegroundColor = ConsoleColor.DarkYellow;
+                            Console.WriteLine("---------------------------------------------------------------------------------------------");
+                            Console.ResetColor();
+                            Console.WriteLine();
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("File Not Found");
+                            Console.ResetColor();
+                        }
+
+                        break;
+
+                    case 3:
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        Console.WriteLine("                           Enter '3' to View Particular Employee                             ");
+                        Console.ResetColor();
+                        Console.WriteLine();
+                        Console.ForegroundColor = ConsoleColor.DarkCyan;
+                        Console.WriteLine("---------------------------------------------------------------------------------------------");
+                        Console.ResetColor();
+                        Console.WriteLine();
+
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write("Enter the Employee ID : ");
+                        Console.ResetColor();
+                        string Empid = Console.ReadLine();
+                        Console.WriteLine();
+
+                        Console.WriteLine();
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        Console.WriteLine($"                  Here are the Employee details for Employee - {Empid}                      ");
+
+
+
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        Console.WriteLine("---------------------------------------------------------------------------------------------");
+                        Console.ResetColor();
+
+                        Console.WriteLine(
+                            $"{"Employee ID",-20}" +
+                            $"{"Employee Name",-25}" +
+                            $"{"Department",-20}" +
+                            $"{"Salary",-15}"
+                        );
+
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        Console.WriteLine("---------------------------------------------------------------------------------------------");
+                        Console.ResetColor();
+
+                        foreach (string list in File.ReadLines(FilePath))
+                        {
+                            string[] EmpDetails = list.Split(',');
+
+                            if (EmpDetails[0] == Empid)
+                            {
+                                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                                Console.WriteLine(
+                                       $"{EmpDetails[0],-20}" +
+                                       $"{EmpDetails[1],-25}" +
+                                       $"{EmpDetails[2],-20}" +
+                                       $"{EmpDetails[3],-15}"
+                                );
+                                Console.ResetColor();
+                            }
+                            else
+                            {
+                                continue;
+                            }
+                        }
+
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        Console.WriteLine("---------------------------------------------------------------------------------------------");
+                        Console.ResetColor();
+
+                        Console.WriteLine();
+
+                        break;
+
+                    
+
+                }
             }
         }
     }
